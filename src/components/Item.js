405f9cc6a95fdced6ae1b2d13477e2ac7cf4c8d7
sidee <said.issack@student.moringaschool.com>
2023-07-20 22:26:37 +0300

    added the darkmode

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  const handleAddToCart = () => {
+    // Toggle the isInCart state when the button is clicked
+    setIsInCart(!isInCart);
+  };
+
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>
+        {isInCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
